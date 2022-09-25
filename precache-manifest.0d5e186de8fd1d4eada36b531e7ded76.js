741e4991d447d4e048881a78bd1353e983d076df
Code Sandbox <hello@codesandbox.io>
2022-09-25 00:38:38 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0d5e186de8fd1d4eada36b531e7ded76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9752e921b635410aec4cd04adf230f4",
+    "url": "https://k0gasa.github.io/csb-31shld/index.html"
+  },
+  {
+    "revision": "97593bc4e0d3e69cbcf0",
+    "url": "https://k0gasa.github.io/csb-31shld/static/css/2.bd37aef8.chunk.css"
+  },
+  {
+    "revision": "97593bc4e0d3e69cbcf0",
+    "url": "https://k0gasa.github.io/csb-31shld/static/js/2.21f920bf.chunk.js"
+  },
+  {
+    "revision": "6dc75be78fe8ccbf8ba1",
+    "url": "https://k0gasa.github.io/csb-31shld/static/js/main.d0f4ab0e.chunk.js"
+  },
+  {
+    "revision": "a33fe13176825d2bf424",
+    "url": "https://k0gasa.github.io/csb-31shld/static/js/runtime~main.bd213b8d.js"
+  }
+]);
